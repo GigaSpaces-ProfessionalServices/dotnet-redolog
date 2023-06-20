aad2c768703b5ce38779053f5dfc57233048648b
Jay Dalal <jaydalal_12194@yahoo.com>
2023-06-21 01:51:55 +0530

    Fixed path issue in notifier

--- redolog-shutdown-notify/src/main/java/org/example/RedologFlushWorker.java
+++ redolog-shutdown-notify/src/main/java/org/example/RedologFlushWorker.java
@@ -14,17 +14,18 @@ public class RedologFlushWorker implements RedologFlushNotifier {
     public void notifyOnFlush(String fullSpaceName, String spaceName, long redologsize, Path redologPath) {
         System.out.println("==========================================");
         System.out.println("notifyOnFlush was called with: fullSpaceName=" + fullSpaceName + " spaceName=" + spaceName + " redologSize=" + redologsize + " redologPath=" + redologPath.toFile().getAbsolutePath());
-        System.out.println("scriptLocation : " + System.getProperty("scriptLocation"));
         System.out.println("redologBackupPath : " + System.getProperty("redologBackupPath"));
-        //String[] args = {"RemoteCopyfiles","--scriptLocation=C:\\GigaSpaces\\smart-cache.net-16.2.1-x64\\NET v4.0\\automation-redo-log\\ ","--spaceName=dataExampleSpace","--redologBackupPath=C:\\GigaSpaces\\smart-cache.net-16.2.1-x64\\NET v4.0\\backup\\work\\redo-log ","--sourceBaseDirCopyPath=C:\\GigaSpaces\\smart-cache.net-16.2.1-x64\\NET v4.0\\Work\\redo-log\\ "};
-        String[] args = {"RemoteCopyfiles", "--scriptLocation=" + System.getProperty("scriptLocation"), "--spaceName=" + System.getProperty("spaceName"), "--redologBackupPath=" + System.getProperty("redologBackupPath"), "--sourceBaseDirCopyPath=" + System.getProperty("sourceBaseDirCopyPath"), "--hostFileName=" + System.getProperty("hostFileName")};
+        //String[] args = {"RemoteCopyfiles","--scriptLocation=C:\\GigaSpaces\\XAP.NET-16.3.0-patch-p-3-x64\\NET v4.0\\automation-redo-log\\ ","--spaceName=dataExampleSpace","--redologBackupPath=C:\\GigaSpaces\\XAP.NET-16.3.0-patch-p-3-x64\\NET v4.0\\backup\\work\\redo-log ","--sourceBaseDirCopyPath=C:\\GigaSpaces\\XAP.NET-16.3.0-patch-p-3-x64\\NET v4.0\\Work\\redo-log\\ "};
+        String[] args = {"RemoteCopyfiles","--configLocation=" + System.getProperty("configLocation"), "--resourceLocation=" + System.getProperty("resourceLocation"), "--spaceName=" + System.getProperty("spaceName"), "--redologBackupPath=" + System.getProperty("redologBackupPath"), "--sourceBaseDirCopyPath=" + System.getProperty("sourceBaseDirCopyPath"), "--hostFileName=" + System.getProperty("hostFileName")};
         RemoteCommandExecutor.main(args);
     }
 
     public static void main(String[] arg1s) {
-        String[] args = {"RemoteCopyfiles","--scriptLocation=C:\\GigaSpaces\\smart-cache.net-16.2.1-x64\\NET v4.0\\automation-redo-log\\ ","--spaceName=dataExampleSpace","--redologBackupPath=C:\\GigaSpaces\\smart-cache.net-16.2.1-x64\\NET v4.0\\backup\\work\\redo-log ","--sourceBaseDirCopyPath=C:\\GigaSpaces\\smart-cache.net-16.2.1-x64\\NET v4.0\\Work\\redo-log\\ ", "--hostFileName=spaceHosts.txt"};
+        String[] args = {"RemoteCopyfiles","--configLocation=C:\\GigaSpaces\\XAP.NET-16.3.0-patch-p-3-x64\\NET v4.0\\RecoveryAssistant\\config\\ ","--resourceLocation=C:\\GigaSpaces\\XAP.NET-16.3.0-patch-p-3-x64\\NET v4.0\\RecoveryAssistant\\resources\\ ","--spaceName=dataExampleSpace","--redologBackupPath=C:\\GigaSpaces\\XAP.NET-16.3.0-patch-p-3-x64\\NET v4.0\\backup\\work\\redo-log ","--sourceBaseDirCopyPath=C:\\GigaSpaces\\XAP.NET-16.3.0-patch-p-3-x64\\NET v4.0\\Work\\redo-log\\ ", "--hostFileName=spaceHosts.json"};
+        //String[] args = {"RemoteCopyfiles","--scriptLocation=C:\\GigaSpaces\\XAP.NET-16.3.0-patch-p-3-x64\\NET v4.0\\automation-redo-log\\ ","--spaceName=dataExampleSpace","--redologBackupPath=C:\\GigaSpaces\\XAP.NET-16.3.0-patch-p-3-x64\\NET v4.0\\backup\\work\\redo-log ","--sourceBaseDirCopyPath=C:\\GigaSpaces\\XAP.NET-16.3.0-patch-p-3-x64\\NET v4.0\\Work\\redo-log\\ ", "--hostFileName=spaceHosts.txt"};
         //String[] args = {"RemoteCopyfiles", "--scriptLocation=" + System.getProperty("scriptLocation"), "--spaceName=" + System.getProperty("spaceName"), "--redologBackupPath=" + System.getProperty("redologBackupPath"), "--sourceBaseDirCopyPath=" + System.getProperty("sourceBaseDirCopyPath"), "--hostFileName=" + System.getProperty("hostFileName")};
         RemoteCommandExecutor.main(args);
     }
 
 }
+
